1d2506b0defa000a77b658063eb165fe5f417797
dor-95 <diego.rocha@edu.unipar.br>
2023-10-22 22:25:54 -0300

    feat: service layer implementation

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package me.dio.domain.repository;
 
+import me.dio.domain.model.Account;
 import me.dio.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+
 }
