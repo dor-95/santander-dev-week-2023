2dd04f1847ca86504001c152e8cbb513a585e90e
dor-95 <diego.rocha@edu.unipar.br>
2023-10-23 23:32:13 -0300

    feat: adding Swagger specs

--- src/main/java/me/dio/domain/controller/UserController.java
+++ src/main/java/me/dio/domain/controller/UserController.java
@@ -1,5 +1,9 @@
 package me.dio.domain.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.dio.domain.model.User;
 import me.dio.domain.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users Controller", description = "RESTful API for managing users.")
 public class UserController {
 
     private final UserService userService;
@@ -19,12 +24,22 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a user by ID", description = "Retrieve a specific user based on its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation successful"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<User> findById(@PathVariable Long id) {
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid user data provided")
+    })
     public ResponseEntity<User> create(@RequestBody User userToCreate) {
         User userCreated = userService.create(userToCreate);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
