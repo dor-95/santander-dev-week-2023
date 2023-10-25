6c7b431a4bd591a33b7590a666cf2e158b9afe6e
dor-95 <diego.rocha@edu.unipar.br>
2023-10-24 23:23:23 -0300

    feat: added builder design pattern

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -2,6 +2,8 @@ package me.dio.domain.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "tb_user")
@@ -71,5 +73,99 @@ public class User {
 
     public void setNews(List<News> news) {
         this.news = news;
+    }
+
+
+    public static final class UserBuilder {
+        private Long id;
+        private String name;
+        private Account account;
+        private Card card;
+        private List<Feature> features;
+        private List<News> news;
+
+        public UserBuilder() {
+            this.account = new Account();
+            this.card = new Card();
+            this.features = new ArrayList<>();
+            this.news = new ArrayList<>();
+        }
+
+        public UserBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder addAccountId(Long id) {
+            this.account.setId(id);
+            return this;
+        }
+
+        public UserBuilder addAccountNumber(String number) {
+            this.account.setNumber(number);
+            return this;
+        }
+
+        public UserBuilder addAccountAgency(String agency) {
+            this.account.setAgency(agency);
+            return this;
+        }
+
+        public UserBuilder addAccountBalance(BigDecimal balance) {
+            this.account.setBalance(balance);
+            return this;
+        }
+
+        public UserBuilder addAccountLimit(BigDecimal limit) {
+            this.account.setLimit(limit);
+            return this;
+        }
+
+        public UserBuilder addCardId(Long id) {
+            this.card.setId(id);
+            return this;
+        }
+
+        public UserBuilder addCardNumber(String number) {
+            this.card.setNumber(number);
+            return this;
+        }
+
+        public UserBuilder addCardLimit(BigDecimal limit) {
+            this.card.setLimit(limit);
+            return this;
+        }
+
+        public UserBuilder addFeature(String icon, String description) {
+            Feature feature = new Feature();
+            feature.setIcon(icon);
+            feature.setDescription(description);
+            this.features.add(feature);
+            return this;
+        }
+
+        public UserBuilder addNews(String icon, String description) {
+            News news = new News();
+            news.setIcon(icon);
+            news.setDescription(description);
+            this.news.add(news);
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.setId(id);
+            user.setName(name);
+            user.setAccount(account);
+            user.setCard(card);
+            user.setFeatures(features);
+            user.setNews(news);
+            return user;
+        }
     }
 }
